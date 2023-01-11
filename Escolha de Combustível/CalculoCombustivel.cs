7804f6766978870cb0235ec7b5f1857f6212db58
Marcus Vinicius Thomé <71160051+marcusvthome@users.noreply.github.com>
2023-01-10 22:19:00 -0300

    Add files via upload

---
+++ Escolha de Combustível/CalculoCombustivel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escolha_de_Combustível
+{
+    public abstract class CalculoCombustivel
+    {
+        public static string CalcularCombustivel(double ValorEtanol, double ValorGasolina, double Consumo)
+        {
+            if (ValorEtanol < (ValorGasolina * Consumo))
+            {
+                return "O combustível ideal é o Etanol";
+            }
+            else if (ValorEtanol == (ValorGasolina * Consumo))
+            {
+                return "O combustível pode ser Gasolina ou Etanol";
+
+            } else {
+                return "O combustível ideal é a Gasolina";
+            }
+        }
+
+        public static double CalcularConsumo(double ConsumoEtanol, double ConsumoGasolina)
+        {
+            return ConsumoEtanol / ConsumoGasolina;
+        }
+    }
+}
+
